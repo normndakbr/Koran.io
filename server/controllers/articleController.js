4bce71b73d4537500e6da69cab3f50f0b24be815
normndakbr <normnd.akbr2@gmail.com>
2021-03-12 09:25:30 +0700

    added articleController and prepare article.json

--- server/controllers/articleController.js
+++ server/controllers/articleController.js
@@ -1,1 +1,19 @@
 const { Article } = require('../models')
+
+class articleController {
+    static async showArticle(request, response, next){
+        try {
+            const userId = request.loggedInUser.id;
+            const data = await Article.findAll()
+
+            response.status(200).json(data);
+        } catch (error) {
+            console.log(error);
+            next(error)
+        }
+    }
+
+    
+}
+
+module.exports = articleController
